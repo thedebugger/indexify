82624a6928b157ce926e1a60dea48a2c3f843eb8
Diptanu Gon Choudhury <diptanuc@gmail.com>
2024-11-01 22:23:07 -0700

    Exporting server metrics

--- server/src/service.rs
+++ server/src/service.rs
@@ -3,6 +3,8 @@ use std::{net::SocketAddr, path::Path, sync::Arc};
 use anyhow::{anyhow, Result};
 use axum_server::Handle;
 use blob_store::BlobStorage;
+use opentelemetry::{global, KeyValue};
+use opentelemetry_sdk::{metrics::SdkMeterProvider, Resource};
 use state_store::{
     kv::KVS,
     requests::{NamespaceRequest, RequestPayload::CreateNameSpace, StateMachineUpdateRequest},
@@ -21,6 +23,22 @@ use crate::{
 };
 
 const DEFAULT_NAMESPACE: &str = "default";
+
+fn init_meter_provider() -> Result<prometheus::Registry> {
+    let registry = prometheus::Registry::new();
+    let exporter = opentelemetry_prometheus::exporter()
+        .with_registry(registry.clone())
+        .build()?;
+    let provider = SdkMeterProvider::builder()
+        .with_reader(exporter)
+        .with_resource(Resource::new([KeyValue::new(
+            "service.name",
+            "indexify-server",
+        )]))
+        .build();
+    global::set_meter_provider(provider.clone());
+    Ok(registry)
+}
 
 pub struct Service {
     pub config: ServerConfig,
@@ -47,12 +65,14 @@ impl Service {
         let kvs_manifest_path = kvs_manifest_path
             .to_str()
             .ok_or(anyhow!(format!("unable to create kv store path")))?;
+        let metrics_registry = Arc::new(init_meter_provider()?);
         let kvs = KVS::new(&kvs_manifest_path.to_string()).await?;
         let route_state = RouteState {
             indexify_state: indexify_state.clone(),
             kvs: Arc::new(kvs),
             blob_storage: blob_storage.clone(),
             executor_manager,
+            registry: metrics_registry,
         };
         let app = create_routes(route_state);
         let handle = Handle::new();
